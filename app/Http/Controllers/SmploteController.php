95df23f772e9052887975c96318d1d4e615d9a78
dlg <wuj@igancao.com>
2022-06-24 09:48:44 +0800

    index route

---
+++ app/Http/Controllers/SmploteController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SmploteController extends Controller {
+
+    /**
+     * smplote index
+     *
+     * @return string
+     * <li> true </li>
+     * @author wuj@igancao.com
+     * @date 2022/06/24 09:44
+     */
+    public function index(): string
+    {
+        return 'welcome to smplote';
+    }
+
+}

ccdf524b66b0cdb6174961096c8c8310e67b6f12
dlg <wuj@igancao.com>
2022-06-24 09:46:14 +0800

    index route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [\App\Http\Controllers\SmploteController::class, 'index']);
+
+Route::prefix('v1')->group(function () {
+    Route::get('index', [\App\Http\Controllers\SmploteController::class, 'index']);
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
